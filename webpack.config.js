342a44f8175a5799c7165d996db43047eb31663e
Scatenarafael <rafascatena@gmail.com>
2021-05-04 21:38:11 -0300

    Atualização com a configuração do Typescript no babel e webpack

--- webpack.config.js
+++ webpack.config.js
@@ -8,14 +8,14 @@ module.exports = {
   mode: isDevelopment ? 'development' : 'production',
   devtool: isDevelopment ? 'eval-source-map' : 'source-map', //em devtools, os source-maps permitem uma organização melhor do arquivo bundle.js gerando uma
   //vizualização mais clara dos scripts na aba source do devtools do navegador
-  entry: path.resolve(__dirname, 'src', 'index.jsx'), //arquivo principal da aplicação
+  entry: path.resolve(__dirname, 'src', 'index.tsx'), //arquivo principal da aplicação
   //(é importante usar o path.resolve pois a barra muda em diferentes sistemas operacionais)
   output: {//definição do arquivo gerado com o webpack
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
   resolve: {//define as extensões de arquivos que  webpack será capaz de ler
-    extensions: ['.js', '.jsx'],
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
   },
   devServer: {
     contentBase: path.resolve(__dirname, 'public'),
@@ -31,7 +31,7 @@ module.exports = {
     //cada uma das extensões de arquivos
     rules: [//Array de regras com 1 objeto para cada tipo de arquivo
       {
-        test: /\.jsx$/,//expressão regular para determinar se o arquivo é javascript ou não
+        test: /\.(j|t)sx$/,//expressão regular para determinar se o arquivo é javascript ou não
         exclude: /node_modules/,//exclui a necessidade de aplicar o webpack nos arquivos da pasta node_modules
         //pois os arquivos desta pasta ja estão preparados para serem lidos pelo browser
         use: {
